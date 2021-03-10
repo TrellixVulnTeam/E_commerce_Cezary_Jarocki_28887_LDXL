129a43ed18148c30a263f1c7c758f50205630ff8
FunnyPaper <78097248+FunnyPaper@users.noreply.github.com>
2021-03-10 09:10:52 +0100

    Dodano routing, landing i products

--- myshop/src/app/app.module.ts
+++ myshop/src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { ProductsComponent } from './components/products/products.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/landing', pathMatch: 'full'},
+  { path: 'landing', component: LandingComponent},
+  { path: 'products', component: ProductsComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
